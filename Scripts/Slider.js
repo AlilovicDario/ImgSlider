5f7e336dd48973c76543f2f701a50e2034bade50
dalilovic <darioalilovic214@gmail.com>
2022-01-03 17:27:58 +0100

    Improved js using jquery (no longer dependent on image id), recreated styles using scss, other minor fixes.

--- Scripts/Slider.js
+++ Scripts/Slider.js
@@ -1,7 +1,5 @@
+//korištena sintaksa je podržana i u ES5 i u ES6 pa nije potreban transpile
 $(document).ready(function () {
-    var images1 = document.getElementById('picture-row-1').children;
-    var images2 = document.getElementById('picture-row-2').children;
-
     var delay = 1000;
 
     $("#left").click(function () {
@@ -23,54 +21,36 @@ $(document).ready(function () {
         setTimeout(function () {
             $("#left").prop("disabled", false);
             $("#right").prop("disabled", false);
-        },delay + 10);
+        }, delay + 10);
     }
 
 
     function topRowLeft() {
-        for (var i = 0; i <= images1.length - 1; i++) {
-            $("#" + images1[i].id).css("right", -$(images1[images1.length - 1]).width() - 10);
-        }
-        $("#" + images1[images1.length - 1].id).insertBefore($("#" + images1[0].id));
-        for (var i = 0; i <= images1.length - 1; i++) {
-            $("#" + images1[i].id).animate({ right: 0 }, delay);
-        }
+        $("#picture-row-1").children().css("right", -$("#picture-row-1").children().last().width() - 10).promise().done(function () {
+            $("#picture-row-1").children().last().insertBefore($("#picture-row-1").children().first());
+            $("#picture-row-1").children().animate({ right: 0 }, delay);
+        });
+
     }
 
     function bottomRowLeft() {
-        for (var i = 0; i <= images2.length - 1; i++) {
-            $("#" + images2[i].id).css("right", -$(images2[images2.length - 1]).width() - 10);
-        }
-        $("#" + images2[images2.length - 1].id).insertBefore($("#" + images2[0].id));
-        for (var i = 0; i <= images2.length - 1; i++) {
-            $("#" + images2[i].id).animate({ right: 0 }, delay);
-        }
+        $("#picture-row-2").children().css("right", -$("#picture-row-2").children().last().width() - 10).promise().done(function () {
+            $("#picture-row-2").children().last().insertBefore($("#picture-row-2").children().first());
+            $("#picture-row-2").children().animate({ right: 0 }, delay);
+        });
     }
 
     function topRowRight() {
-        var last = "#" + images1[images1.length - 1].id;
-        var first = "#" + images1[0].id;
-        for (var i = 0; i <= images1.length - 1; i++) {
-            $("#" + images1[i].id).animate({ right: -$(images1[0]).width() - 10 }, delay, function () {
-                for (var i = 0; i <= images1.length - 1; i++) {
-                    $("#" + images1[i].id).css("right", 0);
-                    $(first).insertAfter($(last));
-                }
-            });
-        }
+        $("#picture-row-1").children().animate({ right: -$("#picture-row-1").children().first().width() - 10 },delay).promise().done(function(){
+            $("#picture-row-1").children().first().insertAfter($("#picture-row-1").children().last());
+            $("#picture-row-1").children().css("right", 0);
+        });
     }
 
     function bottomRowRight() {
-        var last = "#" + images2[images2.length - 1].id;
-        var first = "#" + images2[0].id;
-        for (var i = 0; i <= images2.length - 1; i++) {
-            $("#" + images2[i].id).animate({ right: -$(images2[0]).width() - 10 }, delay, function () {
-                for (var i = 0; i <= images2.length - 1; i++) {
-                    $("#" + images2[i].id).css("right", 0);
-                    $(first).insertAfter($(last));
-                }
-            });
-        }
+        $("#picture-row-2").children().animate({ right: -$("#picture-row-2").children().first().width() - 10 },delay).promise().done(function(){
+            $("#picture-row-2").children().first().insertAfter($("#picture-row-2").children().last());
+            $("#picture-row-2").children().css("right", 0);
+        });
     }
-
 });
