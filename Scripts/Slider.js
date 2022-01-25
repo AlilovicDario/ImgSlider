9d8cfd5f4a1894360814858d20fef900a80c7dcc
dalilovic <darioalilovic214@gmail.com>
2022-01-25 17:58:00 +0100

    Changed js syntax to ES6, updated sass file and enabled babel for tranispileing

--- Scripts/Slider.js
+++ Scripts/Slider.js
@@ -1,56 +1,39 @@
-//korištena sintaksa je podržana i u ES5 i u ES6 pa nije potreban transpile
-$(document).ready(function () {
-    var delay = 1000;
+$(document).ready(() => {
+    const delay = 1000;
 
-    $("#left").click(function () {
+    $("#left").click(() => {
         disableBtns();
-        topRowLeft();
-        bottomRowLeft();
+        moveLeft("#picture-row-1");
+        moveLeft("#picture-row-2");
     });
 
-    $("#right").click(function () {
+    $("#right").click(() => {
         disableBtns();
-        topRowRight();
-        bottomRowRight();
+        moveRight("#picture-row-1");
+        moveRight("#picture-row-2");
     });
 
-    function disableBtns() {
+    let disableBtns = () => {
         $("#left").prop("disabled", true);
         $("#right").prop("disabled", true);
 
-        setTimeout(function () {
+        setTimeout(() => {
             $("#left").prop("disabled", false);
             $("#right").prop("disabled", false);
         }, delay + 10);
     }
 
+    let moveLeft = (row) => $(row).children().css("right", -$(row).children().last().width() - 10).promise().done(() => moveBefore(row));
 
-    function topRowLeft() {
-        $("#picture-row-1").children().css("right", -$("#picture-row-1").children().last().width() - 10).promise().done(function () {
-            $("#picture-row-1").children().last().insertBefore($("#picture-row-1").children().first());
-            $("#picture-row-1").children().animate({ right: 0 }, delay);
-        });
+    let moveRight = (row) => $(row).children().animate({ right: -$(row).children().first().width() - 10 }, delay).promise().done(() => moveAfter(row));
 
+    let moveBefore = (row) => {
+        $(row).children().last().insertBefore($(row).children().first());
+        $(row).children().animate({ right: 0 }, delay);
     }
 
-    function bottomRowLeft() {
-        $("#picture-row-2").children().css("right", -$("#picture-row-2").children().last().width() - 10).promise().done(function () {
-            $("#picture-row-2").children().last().insertBefore($("#picture-row-2").children().first());
-            $("#picture-row-2").children().animate({ right: 0 }, delay);
-        });
-    }
-
-    function topRowRight() {
-        $("#picture-row-1").children().animate({ right: -$("#picture-row-1").children().first().width() - 10 },delay).promise().done(function(){
-            $("#picture-row-1").children().first().insertAfter($("#picture-row-1").children().last());
-            $("#picture-row-1").children().css("right", 0);
-        });
-    }
-
-    function bottomRowRight() {
-        $("#picture-row-2").children().animate({ right: -$("#picture-row-2").children().first().width() - 10 },delay).promise().done(function(){
-            $("#picture-row-2").children().first().insertAfter($("#picture-row-2").children().last());
-            $("#picture-row-2").children().css("right", 0);
-        });
+    let moveAfter = (row) => {
+        $(row).children().first().insertAfter($(row).children().last());
+        $(row).children().css("right", 0);
     }
 });
